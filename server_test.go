4d3bed0cb99a8d04479c2a98434d25f5345a6a15
mas9612 <mas9612@gmail.com>
2018-08-27 00:46:44 +0900

    add test for hostsHandler

---
+++ server_test.go
@@ -0,0 +1,106 @@
+package ipmonitor
+
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/jinzhu/gorm"
+	// for gorm to use sqlite
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+)
+
+type TestHostsValue struct {
+	Method string
+	Input  TestHostRequest
+	Expect TestHostResponse
+}
+
+type TestHostRequest struct {
+	Address     string `json:"address"`
+	Hostname    string `json:"hostname"`
+	Description string `json:"description"`
+}
+
+type TestHostResponse struct {
+	StatusCode int
+	HostsResponse
+}
+
+func TestMain(m *testing.M) {
+	const DBfile = "unittest.db"
+	db, err := gorm.Open("sqlite3", DBfile)
+	if err != nil {
+		log.Fatalln("Failed to open DB")
+	}
+	defer db.Close()
+
+	db.AutoMigrate(&Host{})
+
+	res := m.Run()
+	err = os.Remove(DBfile)
+	if err != nil {
+		log.Println(err)
+	}
+	os.Exit(res)
+}
+
+func TestHostsHandler(t *testing.T) {
+	// TODO: add test case if error returned
+	router := NewHTTPHandler()
+
+	values := []TestHostsValue{
+		TestHostsValue{
+			Method: "GET",
+			Input:  TestHostRequest{},
+			Expect: TestHostResponse{http.StatusOK, HostsResponse{Count: 0, Hosts: []Host{}}},
+		},
+		TestHostsValue{
+			Method: "POST",
+			Input:  TestHostRequest{Address: "10.1.240.151", Hostname: "k8s-01", Description: "k8s node #1"},
+			Expect: TestHostResponse{http.StatusCreated, HostsResponse{Count: 1, Hosts: []Host{Host{Address: "10.1.240.151", Hostname: "k8s-01", Description: "k8s node #1"}}}},
+		},
+		TestHostsValue{
+			Method: "GET",
+			Input:  TestHostRequest{},
+			Expect: TestHostResponse{http.StatusOK, HostsResponse{Count: 1, Hosts: []Host{Host{Address: "10.1.240.151", Hostname: "k8s-01", Description: "k8s node #1"}}}},
+		},
+	}
+
+	for _, v := range values {
+		payload, _ := json.Marshal(v.Input)
+		rr := httptest.NewRecorder()
+		var req *http.Request
+		if v.Method == "GET" {
+			req = httptest.NewRequest(v.Method, "/hosts", nil)
+		} else {
+			req = httptest.NewRequest(v.Method, "/hosts", bytes.NewReader(payload))
+		}
+		router.ServeHTTP(rr, req)
+
+		res := rr.Result()
+		if res.StatusCode != v.Expect.StatusCode {
+			t.Errorf("GET /hosts: Wrong status code was returned. '%d' != '%d'", res.StatusCode, v.Expect.StatusCode)
+		}
+	}
+}
+
+func TestHostHandler(t *testing.T) {
+
+}
+
+func checkHostsResponse(t *testing.T, res, expected Host) {
+	if res.Address != expected.Address {
+		t.Errorf("GET /hosts: Wrong Address was returned. '%s' != '%s'", res.Address, expected.Address)
+	}
+	if res.Hostname != expected.Hostname {
+		t.Errorf("GET /hosts: Wrong Hostname was returned. '%s' != '%s'", res.Hostname, expected.Hostname)
+	}
+	if res.Description != expected.Description {
+		t.Errorf("GET /hosts: Wrong Description was returned. '%s' != '%s'", res.Description, expected.Description)
+	}
+}
